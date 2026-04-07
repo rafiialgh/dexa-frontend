9400da409cd1bd1ac77214f7a96df2bddd8298da
rafiialgh <rafii.alghafari@gmail.com>
2026-04-08 00:29:27 +0700

    refactor: remove unused import from utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 import { useState, useEffect } from "react"
-import { format } from "date-fns"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
